16f7d97c8a7d3443570edb886ffd48e33c1abe54
StasAlex <stas.alex28@gmail.com>
2019-11-01 17:38:10 +0200

    section Article section was made

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    //--------- Main Nav hover effects --------//
     $('.main_nav a').hover(function() {
         $( '.main_nav a' ).removeClass('active_link');
         $( this ).addClass('active_link');
@@ -8,16 +10,52 @@ $(document).ready(function(){
         $( this ).addClass('active_link');
     });
 
-    $(".main_slider").owlCarousel({
+
+    //--------- Main Slider init --------//
+    const main_slider = $(".main_slider");
+
+    main_slider.owlCarousel({
         items: 1,
         loop: true,
         autoplay: true,
+        dots: false,
         smartSpeed: 1000,
         autoplayTimeout: 3000,
-        nav: true,
+        autoplayHoverPause: true
+    });
+
+
+    $('.main_slider-prev').click(function() {
+        main_slider.trigger('prev.owl.carousel');
+    })
+
+    $('.main_slider-next').click(function() {
+        main_slider.trigger('next.owl.carousel');
+    })
+
+
+
+    //--------- Articles Slider init --------//
+    const articles_slider = $(".articles_slider");
+
+    articles_slider.owlCarousel({
+        items: 3,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 28,
+        autoplayTimeout: 3000,
         dots: false,
         autoplayHoverPause: true
       });
+
+      $('.articles_slider-prev').click(function() {
+        articles_slider.trigger('prev.owl.carousel');
+    })
+
+    $('.articles_slider-next').click(function() {
+        articles_slider.trigger('next.owl.carousel');
+    })
     
 
 
